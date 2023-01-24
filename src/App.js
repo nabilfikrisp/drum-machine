823eb243964da5ad9cf7418f5c4c051955ab845b
nabilfikrisp <mnabilfikrisp@gmail.com>
2023-01-24 14:20:28 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import DRUMPADS from './Drumpads';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [activeKey, setActiveKey] = useState('Press the Key')
+
+  useEffect(() => {
+    document.addEventListener('keydown', (event) => {
+      playSound(event.key.toUpperCase())
+    })
+  }, [])
+
+  function playSound(selector) {
+    const audio = document.getElementById(selector);
+    if (audio === null) {
+      console.log("not a key");
+    } else {
+      audio.play();
+      console.log(audio.parentElement);
+      setActiveKey(audio.parentElement.id);
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-teal-500 font-fira text-white">
+      <div
+        className="App flex justify-center items-center h-screen flex-col w-80 mx-auto"
+        id="drum-machine"
+      >
+        <div id="display" className='w-80 bg-teal-400 font-semibold text-xl text-center h-[100px] flex justify-center items-center rounded-md border-2 border-white mb-4'>
+          {activeKey}
+        </div>
+        <div className="drum-pads grid grid-cols-3 w-80 gap-4 bg-transparent font-bold">
+          {DRUMPADS.map((drumPad) => (
+            <div
+              key={drumPad.key}
+              onClick={() => {
+                playSound(drumPad.text);
+              }}
+              className="drum-pad bg-teal-400 border-white text-4xl border-2 w-24 h-24 flex items-center justify-center rounded-md flex-col hover:scale-110 transition-all duration-200 ease-in-out"
+              id={drumPad.key}
+            >
+              {drumPad.text}
+              <p className='text-sm font-bold'>{drumPad.key}</p>
+              <audio
+                className="clip"
+                id={drumPad.text}
+                src={drumPad.src}
+              ></audio>
+            </div>
+          ))}
+        </div>
+        <footer className='mt-4'>created by <a href='https://github.com/nabilfikrisp' className='text-teal-800 font-semibold hover:text-white transition-all duration-300 ease-in-out'>nabilfikrisp</a> with ❤</footer>
+      </div>
     </div>
   );
 }
---
+++ src/Drumpads.js
@@ -0,0 +1,58 @@
+const DRUMPADS = [
+  {
+    keyCode: 81,
+    text: "Q",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    key: "Heater-1"
+  },
+  {
+    keyCode: 87,
+    text: "W",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    key: "Heater-2"
+  },
+  {
+    keyCode: 69,
+    text: "E",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    key: "Heater-3"
+  },
+  {
+    keyCode: 65,
+    text: "A",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    key: "Heater-4"
+  },
+  {
+    keyCode: 83,
+    text: "S",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    key: "Heater-6"
+  },
+  {
+    keyCode: 68,
+    text: "D",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    key: "Dsc_Oh"
+  },
+  {
+    keyCode: 90,
+    text: "Z",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    key: "Kick_n_Hat"
+  },
+  {
+    keyCode: 88,
+    text: "X",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    key: "RP4_KICK"
+  },
+  {
+    keyCode: 67,
+    text: "C",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    key: "Cev_H2"
+  }
+];
+
+export default DRUMPADS;
